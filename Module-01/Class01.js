dad597da40730e09fe8b9ebbee3e866600d45a30
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-08 07:29:05 +0600

    Differences between Low-level & High-level Programming language

---
+++ Module-01/Class01.js
@@ -0,0 +1,39 @@
+//01- What is Programming ? How it work's?
+
+// Ans : Programming is the process of creating a set of instructions that tell a computer how to perform a task. 
+
+
+// 02- Classification of Programming ? 
+
+// Ans: Programming language are basically classified into two main cataegories:
+// 1. Low Level Language
+// 2. High Level Language
+
+
+// 1. Statics of Programming Language
+// 2. Dynamic Programming Language
+
+
+// Low Level vs High Level Programming Language:
+
+/* 
+  High Level Programming Language:
+  
+   1. Programmer Friendly.
+   2. Easy to Understand.
+   3. Easy to Debug
+   4. Easy to Execute.
+   5. Compiler / Interpreter.
+   6. Low memory Efficiency.
+*/
+
+/*
+   Low Level Programming Language:
+
+   1. Machine Friendly.
+   2. Hard to Understand.
+   3. Taugh to Debug.
+   4. Hard to Execute.
+   5. Assembler.
+   6. High memory Efficiency.    
+*/
