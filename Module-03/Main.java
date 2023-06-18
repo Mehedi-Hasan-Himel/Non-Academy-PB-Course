3ec7e1bf60b628efb05efc040a09cbc4b357da53
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-18 09:31:58 +0600

    module 03

---
+++ Module-03/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+   public static void main(String[] args) {
+      System.out.println("The maximun value of Byte: " + Byte.MAX_VALUE);
+      System.out.println("The minimum value of Byte: " + Byte.MIN_VALUE);
+      System.out.println("The maximun value of Short: " + Short.MAX_VALUE);
+      System.out.println("The minimum value of Short: " + Short.MIN_VALUE);
+      System.out.println("The maximun value of Integer: " + Integer.MAX_VALUE);
+      
+   }
+}
+   
+// Path: Module-03/MyClass.java
